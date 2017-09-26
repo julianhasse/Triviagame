d12e70a3ecb9e6379d059c4d29f66b20b936487d
Julian Hasse <info@julianhasse.com>
2017-09-26 18:00:14 -0400

    Added Data to questionBank

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -1,68 +1,68 @@
 var questionBank = [
 
 	{
-		question: 'Which company was established on April 1st, 1976 by Steve Jobs, Steve Wozniak and Ronald Wayne?',
-		answers: ['Apple', 'Atari', 'Microsoft', 'Comodore'],
-		correct: 'Apple'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'What does CPU stand for?',
-		answers: ['Central Processing Unit', 'Computer Personal Unit', 'Central Process Unit', 'Central Processor Unit'],
-		correct: 'Central Processing Unit'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'What does the \"MP\"stand for in MP3?',	
-		answers: ['Multi Pass', 'Micro Point', 'Moving Picture', 'Music Player'],
-		correct: 'Moving Picture'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'How many bytes are in a kilobyte of RAM?',	
-		answers: ['1024', '512', '500', '1000'],
-		correct: '1024'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'The programming language Swift was created to replace what other programming language?',	
-		answers: ['C++', 'Objective_C', 'C#', 'Ruby'],
-		correct: 'Objective_C'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'What amount of bits commonly equals one byte?',	
-		answers: ['2', '1', '8', '64'],
-		correct: '8'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'In the programming language Java, which of these keywords would you put on a variable to make sure it doesn\'t get modified?',	
-		answers: ['Final', 'Static', 'Public', 'Private'],
-		correct: 'Final'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'What is the most preferred image format used for logos in the Wikimedia database?',	
-		answers: ['jpeg', 'svg', 'gif', 'png'],
-		correct: 'svg'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'In web design, what does CSS stand for?',	
-		answers: ['Cascading Style Sheet', 'Corrective Style Sheet', 'Counter Strike Source', 'Computer Style Sheet'],
-		correct: '3'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'What is the domain name for the country Tuvalu?',	
-		answers: ['.tu', '.tv', '.tt', '.tl'],
-		correct: '.tv'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	},
 
 	{
-		question: 'How many kilobytes in one gigabyte?',	
-		answers: ['1000000', '1024', '1000', '1048576'],
-		correct: '.tv'
+		question: 'Question 01',
+		answers: ['option 1', 'option 2', 'option 3', 'option 4'],
+		correct: 'option 1'
 	}
 ];
